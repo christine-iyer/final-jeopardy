23a948d432de5b515c65238e20440580b2c62363
chrisIyer <hellohotbox420@gmail.com>
2023-02-08 19:37:04 -0500

    incorporating the collapsable message has thrown something off.

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,35 +1,35 @@
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Dropdown from 'react-bootstrap/Dropdown';
+import Collapse from 'react-bootstrap/Collapse';
 import styles from './ComponentStyles/ComponentStyles.module.scss'
 export default function Answer({ jeopardyQuest }) {
+const [answerQuestion, setAnswerQuestion] = useState(false)
+  const [open, setOpen] = useState(false);
+  return (
+     <>
+       <Button
+         onClick={() => setOpen(!open)}
+         aria-controls="example-collapse-text"
+         aria-expanded={open}
+       >
+         click
+       </Button>
+       <Collapse in={open}>
+         <div id="example-collapse-text">
+         
+      answer {jeopardyQuest.answer}
 
-     const [answerQuestion, setAnswerQuestion] = useState(false)
-
-     return (
-          <>
-               <Button 
-               className={styles.Button}
-               
-               onClick={() => {
-                    setAnswerQuestion(!answerQuestion)
-               }}
-               >
-               {answerQuestion ? "Hide Answer" : "Reveal Answer"}
-               </Button>
-               {jeopardyQuest && jeopardyQuest.question && answerQuestion ? (
-                    <div> answer {jeopardyQuest.answer}</div>
-                    
-               ) : (
-                    "?"
-               )}
-                         </>
-               )
-          }
+         </div>
+       </Collapse>
+     </>
+   );
+ }
 
 
 
 
 
-    
+
+
+
+
