8aaaf72fb425eac451cdbb081dbcc1bea18afc39
chrisIyer <hellohotbox420@gmail.com>
2023-04-05 17:52:02 -0400

    insignig changes.

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,29 +1,54 @@
-import { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Collapse from 'react-bootstrap/Collapse';
-import styles from './ComponentStyles/ComponentStyles.module.scss'
-export default function Answer({ jeopardyQuest }) {
-const [answerQuestion, setAnswerQuestion] = useState(false)
-  const [open, setOpen] = useState(false);
-  return (
-     <>
-       <Button
-         onClick={() => setOpen(!open)}
-         aria-controls="example-collapse-text"
-         aria-expanded={open}
-       >
-         click
-       </Button>
-       <Collapse in={open}>
-         <div id="example-collapse-text">
+// import { useState } from "react";
+// import Button from "react-bootstrap/Button";
+// import Collapse from 'react-bootstrap/Collapse';
+// import styles from './ComponentStyles/ComponentStyles.module.scss'
+// export default function Answer({ jeopardyQuest }) {
+// const [answerQuestion, setAnswerQuestion] = useState(false)
+//   const [open, setOpen] = useState(false);
+//   return (
+//      <>
+//        <Button
+//          onClick={() => setOpen(!open)}
+//          aria-controls="example-collapse-text"
+//          aria-expanded={open}
+//        >
+//          click
+//        </Button>
+//        <Collapse in={open}>
+//          <div id="example-collapse-text">
          
-      answer {jeopardyQuest.answer}
+//       answer {jeopardyQuest.answer}
 
-         </div>
-       </Collapse>
-     </>
-   );
- }
+//          </div>
+//        </Collapse>
+//      </>
+//    );
+//  }
+
+import { useState } from "react";
+export default function Answer({ jeopardyQuest }) {
+
+     const [answerQuestion, setAnswerQuestion] = useState(false)
+
+     return (
+          <>
+               <button
+               onClick={() => {
+                    setAnswerQuestion(!answerQuestion)
+               }}
+               >
+               {answerQuestion ? "Hide Answer" : "Reveal Answer"}
+               </button>
+               {jeopardyQuest && jeopardyQuest.question && answerQuestion ? (
+                    <div> answer {jeopardyQuest.answer}</div>
+
+               ) : (
+                    ""
+               )}
+                         </>
+               )
+          }
+
 
 
 
